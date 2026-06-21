b55953a02183c14840b6dd5ed2ad2de9514f0c45
GD <glicinia95@gmail.com>
2026-06-21 20:42:22 +0100

    comment out to try taking resources out of managed state

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 #Created this so I can see the resource IDs of what has already been created
 
-output "resource_group_id" {
-  value = azurerm_resource_group.rg.id
-}
+# output "resource_group_id" {
+#   value = azurerm_resource_group.rg.id
+# }
 
-output "storage_account_id" {
-  value = azurerm_storage_account.storage.id
-}
+# output "storage_account_id" {
+#   value = azurerm_storage_account.storage.id
+# }
 
 #To see it run terraform Apply and check the output
