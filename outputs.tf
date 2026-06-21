e9ddccdd25351cd50052489859484eb5a9fabb62
GD <glicinia95@gmail.com>
2026-06-21 17:52:45 +0100

    explanation re output

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,4 @@
 #Created this so I can see the resource IDs of what has already been created
-#I dont need to run re-apply to see it
 
 output "resource_group_id" {
   value = azurerm_resource_group.rg.id
@@ -8,3 +7,5 @@ output "resource_group_id" {
 output "storage_account_id" {
   value = azurerm_storage_account.storage.id
 }
+
+#To see it run terraform Apply and check the output
