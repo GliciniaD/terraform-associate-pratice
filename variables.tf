73e4c9c1f7ab03af4f6a057951294a55106f7f46
GD <glicinia95@gmail.com>
2026-07-18 09:18:50 +0100

    add new variable

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "strg_acc_tier" {
+  description = "storage tier"
+  type        = string
+  default     = "standard"
+}
+
 variable "rg_tags" {
   description = "Tags to apply to the resource group"
   type        = map(string)
